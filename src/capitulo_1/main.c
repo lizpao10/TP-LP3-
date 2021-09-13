a6434e9c94a8df4395e12ee5a0af7f523b51cdfb
carinme <carinme97@fpuna.edu.py>
2021-09-13 15:52:43 -0400

    capitulo

---
+++ src/capitulo_1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "reciprocal.hpp"
+int main (int argc, char **argv)
+{
+int i;i = atoi (argv[1]);
+printf ("The reciprocal of %d is %g\n", i, reciprocal (i));
+return 0;
+}
